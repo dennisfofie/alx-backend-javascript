ec28c31efce5d17af562d146af2f8ec2c4c12132
dennisfofie <dennisbeckham000@gmail.com>
2023-04-19 12:53:49 +0000

    fix correct code fail

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  filename
-) {
-  return Promise.all([
-    await signUpUser(firstName, lastName),
-    await uploadPhoto(filename),
-  ]);
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ])
+    .then((responses) => {
+      responses.forEach((response) => ({
+        status: response.status,
+        value:
+          response.status === 'fulfilled' ? response.status : response.reason,
+      }));
+    })
+    .catch(() => {
+      console.error(Error());
+    });
 }
